5fa4998b29b739a55f51a5f8ae276481604a15bf
Nausicaa <39875475+suuupernova@users.noreply.github.com>
2022-05-07 11:34:48 +0800

    Update 141. Linked List Cycle.js

--- Solution/Link List/141. Linked List Cycle.js
+++ Solution/Link List/141. Linked List Cycle.js
@@ -1,4 +1,4 @@
-//use fast and slow pointer
+//解法1：快慢指针
 //if there is a circle in the linked list, the 2 pointers will meet
 var hasCycle = function(head) {
     let fast=slow=head;
@@ -10,5 +10,45 @@ var hasCycle = function(head) {
         }
     }
     return false;
+    
+};
+
+//解法2：利用数组判断(极度不推荐)
+var hasCycle = function(head) {
+    let res=[];
+    while(head){
+        if(res.includes(head)){
+            return true;
+        }
+        res.push(head);
+        head=head.next;
+    }
+    return false;
+    
+};
+
+//解法3
+var hasCycle = function(head) {
+    while(head){
+        if(head.flag){
+            return true;
+        }
+        head.flag=true;
+        head=head.next;
+    }
+    return false;
+    
+};
+
+//解法4
+//利用JavaScript语言特性
+var hasCycle = function(head) {
+    try{
+        JSON.stringify(head);
+        return false;
+    }
+    catch{
+        return true;
+    }
     
 };
