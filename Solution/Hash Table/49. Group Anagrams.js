bf74e6f12ee2fc62f9d544fdacb302bab7c40cf6
Nausicaa <39875475+suuupernova@users.noreply.github.com>
2022-11-01 22:20:40 +0900

    Create 49. Group Anagrams.js

---
+++ Solution/Hash Table/49. Group Anagrams.js
@@ -0,0 +1,12 @@
+var groupAnagrams = function(strs) {
+    let map=new Map();
+    for(let str of strs){
+        let arr=Array.from(str);
+        arr.sort();
+        let key=arr.toString();
+        let list=map.get(key)?map.get(key):new Array();
+        list.push(str);
+        map.set(key,list);
+    }
+    return Array.from(map.values());
+}
