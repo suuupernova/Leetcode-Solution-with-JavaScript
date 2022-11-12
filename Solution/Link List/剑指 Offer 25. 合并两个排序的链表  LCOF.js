e77a99fee88f190bcb9de088450da7688b7c0865
Nausicaa <39875475+suuupernova@users.noreply.github.com>
2022-11-12 16:43:46 +0900

    Update 剑指 Offer 25. 合并两个排序的链表  LCOF.js

--- Solution/Link List/剑指 Offer 25. 合并两个排序的链表  LCOF.js
+++ Solution/Link List/剑指 Offer 25. 合并两个排序的链表  LCOF.js
@@ -1,3 +1,4 @@
+//1.迭代
 var mergeTwoLists = function(l1, l2) {
     let res=new ListNode(-1);
     let curNode=res;
@@ -15,4 +16,18 @@ var mergeTwoLists = function(l1, l2) {
     curNode.next=l1||l2;
     return res.next;
 
+};
+//2.递归
+var mergeTwoLists = function(l1, l2) {
+    if (l1 === null) {
+        return l2;
+    } else if (l2 === null) {
+        return l1;
+    } else if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
 };
